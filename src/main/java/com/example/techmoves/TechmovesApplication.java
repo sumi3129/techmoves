ec20911ead8a2d564cd407b2dcfdee6d5ed69b02
sumi3129 <sumi.s3j04@gmail.com>
2025-08-02 23:56:50 +0800

    init commit

---
+++ src/main/java/com/example/techmoves/TechmovesApplication.java
@@ -0,0 +1,13 @@
+package com.example.techmoves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmovesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechmovesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/techmoves/TechmovesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techmoves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechmovesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
